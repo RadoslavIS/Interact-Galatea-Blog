3cc50e7b09e251b0eae959c7e439f9eb83290f00
RStoyanov <68754435+RoTronimous@users.noreply.github.com>
2024-03-10 20:26:37 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+    $('.toggle-menu').on('click', function() {
+        $('.nav').toggleClass('showing');
+        $('.nav ul').toggleClass('showing');
+        $('.prev').toggleClass('move-down');
+        $('.next').toggleClass('move-down');
+    });
+    
+    $('.post-wrapper').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2200,
+        prevArrow: $('.prev'),
+        nextArrow: $('.next'),
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+    });
+});
+
+    
+
